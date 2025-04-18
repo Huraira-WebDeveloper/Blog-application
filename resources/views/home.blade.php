872a73c1397f40c5546aac59310ca50f813106c5
huraira-WebDeveloper <hurairakhurshid4@gmail.com>
2025-04-18 18:17:25 +0500

    Refactored code into Blade layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,38 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>My Blog</title>
-    
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    
-    <!-- Custom CSS (we'll add this later) -->
-    <link rel="stylesheet" href="{{ asset('css/custom.css') }}">
-</head>
-<body>
+@extends('layouts.app')
 
-<!-- Navigation Bar -->
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <div class="container">
-        <a class="navbar-brand" href="/">My Blog</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link" href="/">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/posts">Posts</a>
-                </li>
-            </ul>
-        </div>
-    </div>
-</nav>
+@section('title', 'Home | My Blog')
 
+@section('content')
 <!-- Hero Section -->
 <div class="hero bg-light py-5">
     <div class="container text-center">
@@ -41,7 +11,7 @@
     </div>
 </div>
 
-<!-- Posts Grid (Sample Content) -->
+<!-- Posts Grid -->
 <div class="container my-5">
     <div class="row">
         <div class="col-md-4 mb-4">
@@ -54,9 +24,4 @@
         </div>
     </div>
 </div>
-
-<!-- Bootstrap JS + Popper -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-
-</body>
-</html>
+@endsection
